ec22c91057e2803eab5a41a93f5b7f3b04714101
alam265 <mda7095y@gmail.com>
2026-02-12 14:34:52 +0600

    Basic Setup

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from pyexpat import model
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from google import genai
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import os
+from google.genai import types
+from typing import Annotated
 
 load_dotenv()
 app = FastAPI() 
@@ -11,20 +14,80 @@ app = FastAPI()
 
 client = genai.Client(api_key=os.getenv("API_KEY"))
 
+my_config = types.GenerateContentConfig(
+system_instruction=(
+        "You are a concise assistant. "
+        "Always answer the user's question in 2-3 sentences first. "
+        "Only add a 'Details' section if the topic is complex or requires deep explanation."
+        "Try to answer it in bullet points"
+    ),
+    temperature=0.7 
+)
+
+
+# Create a chat session
+chat = client.chats.create(
+    model='gemini-3-flash-preview',
+    config=my_config
+    )
+
+@app.post("/chat")
+def chat_bot(user_input: Annotated[str, Form()]):
+
+    response = chat.send_message(user_input)
+
+    return response.text 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @app.get("/health")
 def healt_check():
     return {
         "message":"All Good"
     }
-
-
-@app.get("/chat")
-def reply():
-    response = client.models.generate_content(
-        model = "gemini-3-flash-preview",
-        contents="Hey whats up"
-    )
-    return response.text 
-
-
-
