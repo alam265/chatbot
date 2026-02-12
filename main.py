6a481dec6fd0c7bab1fa379e6134d0b4fcbefcb2
alam265 <mda7095y@gmail.com>
2026-02-12 19:46:47 +0600

    Updated

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ from google.genai import types
 from typing import Annotated
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+import chromadb
+from chromadb.utils import embedding_functions
+from pypdf import PdfReader
+
+chroma_client = chromadb.PersistentClient(path="./university_db")
+sentence_transformer_ef = embedding_functions.DefaultEmbeddingFunction()
+collection = chroma_client.get_collection(
+    name="university_info", 
+    embedding_function=sentence_transformer_ef
+)
 
 load_dotenv()
 app = FastAPI() 
@@ -31,16 +41,34 @@ system_instruction=(
 chat = client.chats.create(
     model='gemini-3-flash-preview',
     config=my_config
+    
     )
 
 chat_req = []
 
 @app.post("/chat", response_class=HTMLResponse)
 def chat_bot(req: Request, user_input: Annotated[str, Form()]):
+    # Query the DB for the 3 most relevant text chunks
+    results = collection.query(
+        query_texts=[user_input],
+        n_results=3
+    )
+    retrieved_context = "\n\n".join(results['documents'][0])
+    augmented_message = f"""
+        You are a helpful assistant for my university.
+        Use the following retrieved context to answer the question below. 
+        If the answer isn't in the context, say you don't know.
+
+        CONTEXT FROM DATABASE:
+        {retrieved_context}
+
+        USER QUESTION:
+        {user_input}
+        """
 
     chat_req.append(user_input)
     
-    response = chat.send_message(user_input)
+    response = chat.send_message(augmented_message)
     
     chat_req.append(response.text)
 
